92c244417215aa755b51fdf5b26c6a6c150349e2
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-12 20:02:10 +1100

    Add trick

--- src/tips_and_tricks.py
+++ src/tips_and_tricks.py
@@ -10,3 +10,19 @@ z = {'a': 1, 'b': 3, 'c': 4}
 x = {'a': 1, 'b': 2}
 y = {'b': 3, 'c': 4}
 z = {**x, **y}
+
+""" testing multiple flags """
+x, y, z = 0, 1, 0
+
+if x == 1 or y == 1 or z == 1:
+    print('passed')
+
+if 1 in (x, y, z):
+    print('passed')
+
+# These only test for truthiness:
+if x or y or z:
+    print('passed')
+
+if any((x, y, z)):
+    print('passed')
