9dcc6b7b5579cf239ca95f8c5a699a551372548c
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-25 10:41:23 +1100

    Add epic notes

--- src/generators/generators.py
+++ src/generators/generators.py
@@ -1,14 +1,34 @@
 import random
+import sys
 
-"""Generators
+"""Generators - a great way to optimise memory
 
 We can create generator expressions without buidling and holding the entire object
 in memory before iteration.
+
+When the yield statement is yit, the program suspends function execution and returns
+the yielded value (return stops execution completely). When a function is suspended, 
+the state of that function is saved. This includes any variable bindings local to the 
+generator, the instruction pointer, the internal stack, and any exception handling.
 """
 
-"""generator instead of list"""
+"""generator vs list
+
+comparing them using sys.getsizeof() - the memory size of object
+"""
 names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
 majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        result.append(person)
+    return result
 
 def people_generator(num_people):
     for i in range(num_people):
@@ -19,7 +39,12 @@ def people_generator(num_people):
                 }
         yield person
 
-people = people_generator(1000000)
+
+people = people_list(1000000)
+sys.getsizeof(people) # 8448728 bytes
+
+people1 = people_generator(1000000)
+sys.getsizeof(people1) # 112 bytes
 
 """reading large files"""
 # bad - f.read().split('\n') loads everything into memory at once
@@ -29,7 +54,7 @@ def csv_reader(file_name):
         return result
 
 # good - open the file loop though each line, yields each row instead of returning 
-def csv_reader2(file_name):
+def csv_reader1(file_name):
     for row in open(file_name, "r"):
         yield row
 
