489f4b92dd47069c049d090adf180adc57a0c83f
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-31 18:03:16 +1100

    Add file

---
+++ src/concurrency/README.md
@@ -0,0 +1,10 @@
+# concurrency in Python
+
+Concurrency means multiple computations are happening at the same time.
+
+Great for I/O bound operations where the CPU is waiting for input/output
+operations to be completed.
+
+CPU bound operations
+import threading - not so great for computational heavy stuff then threads wouldn't be 
+ideal for that type of task.
---
+++ src/concurrency/multithreading/multithreading.py
@@ -0,0 +1,57 @@
+"""multithreading in Python"""
+
+import time
+"""synchronous code snippet"""
+start = time.perf_counter()
+
+# def foo():
+#     print("Sleeping 1 second...")
+#     time.sleep(1)
+#     print('Done sleeping...')
+
+# foo()
+# foo()
+# finish = time.perf_counter()
+
+# print(f'Finished in {round(finish-start, 2)} second(s)') # Finished in 2.01 second(s)
+
+
+# import threading
+# """old way to do threading"""
+# start = time.perf_counter()
+
+# def foo1():
+#     print("Sleeping 1 second...")
+#     time.sleep(1)
+#     print('Done sleeping...')
+
+# threads = []
+# for _ in range(2):
+#     t = threading.Thread(target=foo1)
+#     t.start()
+#     threads.append(t)
+
+# for thread in threads:
+#     thread.join()
+
+# finish = time.perf_counter()
+
+# print(f'Finished in {round(finish-start, 2)} second(s)') # Finished in 1.01 second(s)
+
+
+import concurrent.futures
+"""new way to do threading"""
+start = time.perf_counter()
+
+def foo3(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done Sleeping...{seconds}'
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    results = executor.map(foo3, secs)
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)') # Finished in 5.01 second(s)
