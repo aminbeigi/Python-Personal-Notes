2e35ffc2682459d771fcf6ac9dbbac9f348cc0b4
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-31 21:08:12 +1100

    Fix typo

--- src/concurrency/README.md
+++ src/concurrency/README.md
@@ -1,6 +1,7 @@
 # concurrency in Python
 Concurrency means multiple computations are happening at the same time.
 
+## Multithreading
 Great for I/O bound operations where the CPU is waiting for input/output operations to be completed.
 
 Not so great for CPU bound operations - computational heavy stuff then threads wouldn't be ideal for that type of task.
--- src/generators/generators.py
+++ src/generators/generators.py
@@ -6,7 +6,7 @@ import sys
 We can create generator expressions without buidling and holding the entire object
 in memory before iteration.
 
-When the yield statement is yit, the program suspends function execution and returns
+When the yield statement is hit, the program suspends function execution and returns
 the yielded value (return stops execution completely). When a function is suspended, 
 the state of that function is saved. This includes any variable bindings local to the 
 generator, the instruction pointer, the internal stack, and any exception handling.
