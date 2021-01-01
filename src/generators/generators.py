1145b3b20b36716770ebd43ad8cde216f45af2f0
Amin Ghasembeigi <me@aminbeigi.com>
2021-01-01 11:27:48 +1100

    Move files

--- src/generators/generators.py
+++ src/generators/generators.py
@@ -1,16 +1,7 @@
 import random
 import sys
 
-"""Generators - a great way to optimise memory
-
-We can create generator expressions without buidling and holding the entire object
-in memory before iteration.
-
-When the yield statement is hit, the program suspends function execution and returns
-the yielded value (return stops execution completely). When a function is suspended, 
-the state of that function is saved. This includes any variable bindings local to the 
-generator, the instruction pointer, the internal stack, and any exception handling.
-"""
+"""Generators - a great way to optimise memory"""
 
 """generator vs list
 
